bd4614f2bf36fffd5919bc3cdcbef3ec95e970b9
Maria <tefa1229@gmail.com>
2025-09-11 21:43:30 -0400

    funcionalidades implementadas de carga de galeria, like, eliminar y andir nueva tarjeta

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,124 @@
 const openFormButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button");
-const form = document.querySelector(".popup__form");
-
 const fName = document.querySelector(".profile__name");
 const aboutMe = document.querySelector(".profile__about");
-
 const inputName = document.querySelector("#fname");
 const inputaboutMe = document.querySelector("#aboutMe");
 
-function toggleForm() {
-  popup.classList.add("popup_visible");
-  fName.textContent = inputName.value;
-  aboutMe.textContent = inputaboutMe.value;
+//popup
+const popup = document.querySelector(".popup");
+const closeButtonProfile = document.querySelector(
+  ".popup__profile_close-button"
+);
+const form = document.querySelector(".popup__form");
 
-  fName.value = "";
-  aboutMe.value = "";
+//cards
+const cards = [
+  {
+    name: "Valle de Yosemite",
+    link: "images/yosemite.jpg",
+  },
+  {
+    name: "Colorado",
+    link: "images/colorado.jpg",
+  },
+  {
+    name: "Vail",
+    link: "images/vail.jpg",
+  },
+  {
+    name: "Utah",
+    link: "images/utah.jpg",
+  },
+  {
+    name: "Gran Cañón",
+    link: "images/grandCanyon.jpg",
+  },
+  {
+    name: "Mill Valley",
+    link: "images/millValley.jpg",
+  },
+];
+
+const cardsContainer = document.querySelector(".gallery__container");
+
+//nuevas tarjetas
+const addButton = document.querySelector(".profile__add-button");
+const newCardPopup = document.querySelector(".popup__new-card");
+const closeButtonCard = document.querySelector(
+  ".popup__new-card__close-button"
+);
+const formNewCard = document.forms["new-card"];
+
+//Funciones editar
+function openProfilePopup() {
+  inputName.value = fName.textContent;
+  inputaboutMe.value = aboutMe.textContent;
+
+  popup.classList.add("popup_visible");
 }
 
-openFormButton.addEventListener("click", toggleForm);
-closeButton.addEventListener("click", closepopup);
+openFormButton.addEventListener("click", openProfilePopup);
+closeButtonProfile.addEventListener("click", closeProfilePopup);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   fName.textContent = inputName.value;
   aboutMe.textContent = inputaboutMe.value;
-  closepopup();
+  closeProfilePopup();
+}
+
+function closeProfilePopup() {
+  popup.classList.remove("popup_visible");
 }
 
 form.addEventListener("submit", handleFormSubmit);
 
-function closepopup() {
-  popup.classList.remove("popup_visible");
+// Cards functionality
+
+function createCard(data) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".gallery__card")
+    .cloneNode(true);
+  cardElement.querySelector(".gallery__image").src = data.link;
+  cardElement.querySelector(".gallery__title").textContent = data.name;
+  cardElement
+    .querySelector(".gallery__icon-like")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("gallery__icon-like_active");
+    });
+  cardElement
+    .querySelector(".gallery__icon-delete")
+    .addEventListener("click", function (evt) {
+      evt.target.closest(".gallery__card").remove();
+    });
+  return cardElement;
 }
+
+cards.forEach((card) => {
+  const cardElement = createCard(card);
+  cardsContainer.prepend(cardElement);
+});
+//nueva tarjeta funcionalidad
+
+addButton.addEventListener("click", function () {
+  newCardPopup.classList.add("popup_visible");
+});
+
+formNewCard.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const titleValue = evt.target.title.value;
+  const imageLinkValue = evt.target.imageLink.value;
+  const newCardData = {
+    name: titleValue,
+    link: imageLinkValue,
+  };
+  const cardElement = createCard(newCardData);
+  cardsContainer.prepend(cardElement);
+  closeNewCardPopup();
+});
+
+function closeNewCardPopup() {
+  newCardPopup.classList.remove("popup_visible");
+}
+closeButtonCard.addEventListener("click", closeNewCardPopup);
